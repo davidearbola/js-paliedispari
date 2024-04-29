242d025b7c74bf2dd5b08382c8c1e5faaf164403
davidearbola <davidearbola@gmail.com>
2024-04-29 16:23:22 +0200

    Entrambe le Milestone

--- js/function.js
+++ js/function.js
@@ -1,1 +1,49 @@
-console.log(`io sono function`)
+// PALINDROMA
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma.
+// Sono accettate solo soluzioni che fanno uso del ciclo.
+// Eventuali soluzioni più "comode" potete aggiungere come extra in un commit separato solo DOPO.
+
+// funzione che controlla se la parola dell'utente è palindroma
+function parolaPalindroma(parola) {
+    // variabile appoggio per la parola al contrario
+    let parolaContrario
+    // array per comporre la parola al contrario
+    let array = []
+    // ciclo per selezionare ogni lettera della parola
+    for (let i = parola.length - 1; i >= 0; i--) {
+        // pusho ad ogni ciclo una lettera
+        array.push(parola.charAt(i))
+        // trasformo array in stringa unica
+        parolaContrario = array.join("")
+    }
+    console.log(parolaContrario)
+    // controllo se le parole sono uguali
+    if (parola == parolaContrario){
+        return `E' Palindroma`
+    } else {
+        return `Non è Palindroma`
+    }
+}
+
+// PARI E DISPARI
+// L’utente sceglie su cosa puntare: pari o dispari.
+// L'utente inserisce un numero da 1 a 5.
+// Il computer estrae un numero casuale sempre tra 1 e 5 // (usando una funzione).
+// Stabiliamo se la somma dei due numeri è pari o dispari // (usando una funzione).
+// Dichiariamo chi ha vinto.
+
+// funzione per estrarre un numero casuale da 1 a 5
+function getRandomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// funzione per pari o dispari
+function pariOrDispari (numero1, numero2){
+    let somma = numero1+numero2
+    if (somma % 2 == 0){
+        return `Pari`
+    } else {
+        return `Dispari`
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-console.log(`io sono main`)
+// PALINDROMA
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma.
+// Sono accettate solo soluzioni che fanno uso del ciclo.
+// Eventuali soluzioni più "comode" potete aggiungere come extra in un commit separato solo DOPO.
+
+
+// al click
+let myBtn = document.getElementById(`scopri`)
+myBtn.addEventListener(`click`, function(){
+    // salvo valore input
+    let parolaUtente = document.getElementById(`parola`).value
+    // varibile che prenderà come valore l'output della funzione
+    let risultato = parolaPalindroma(parolaUtente)
+    // stampo risultato
+    document.getElementById(`result`).innerHTML = `La parola ${risultato}`
+})
+
+// PARI E DISPARI
+// L’utente sceglie su cosa puntare: pari o dispari.
+// L'utente inserisce un numero da 1 a 5.
+// Il computer estrae un numero casuale sempre tra 1 e 5 // (usando una funzione).
+// Stabiliamo se la somma dei due numeri è pari o dispari // (usando una funzione).
+// Dichiariamo chi ha vinto.
+
+
+
+// chiedo su cosa punta l'utente
+let puntata = prompt(`Punti su Pari o su Dispari?`)
+// chiedo un numero da 1 a 5
+let numeroUtente = parseInt(prompt(`Scegli un numero da 1 a 5`))
+console.log(`hai scelto: ${puntata}, con il numero ${numeroUtente}`)
+// salvo risultato della funzione numeri random
+let numeroPc = getRandomNumber(1,5)
+console.log(`Il pc ha estratto il numero: ${numeroPc}`)
+// salvo risultato della funzione pari o dispari
+let resPariDispari = pariOrDispari(numeroUtente, numeroPc)
+console.log(`la somme dei due numeri è: ${resPariDispari}`)
+// controllo chi ha vinto se la puntata è uguale al risultato della funzione pari e dispari vince l'utente altrimenti perde
+if (puntata == resPariDispari){
+    console.log(`Hai vinto`)
+} else {
+    console.log(`Hai perso`)
+}
+
